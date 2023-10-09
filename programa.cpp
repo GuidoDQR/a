c952f1e3e6b7d899ba5879a9c00a306af32dfbd8
Guido Diego Quispe Robles <quispeguido2002@gmail.com>
2023-10-09 19:14:15 -0300

    Update programa.cpp
    se cambio el struct NODO, se completo la funcion emitir_reporte y hay valores para probar la funcion

--- programa.cpp
+++ programa.cpp
@@ -11,19 +11,30 @@ struct DATA {
 };
 
 struct NODO {
-    DATA info;
+    int dni;
     NODO *sig;
 };
 
+const int maxFilas = 100;
+const int maxColumnas = 7;
+
 void cargar_archivo(FILE *);
 void leer_archivo(FILE *);
+void procesar_archivo(FILE *, NODO *matriz[maxFilas][maxColumnas]);
+void emitir_reporte(NODO *matriz[maxFilas][maxColumnas]);
+
 int main(){
     setlocale(LC_ALL,"spanish");
     FILE *archivo;
     archivo=fopen("RESERVAS.DAT","wb+");
-
+    
+    // matriz de 100 filas y 7 columnas.
+    NODO* matriz[maxFilas][maxColumnas];
+    
     cargar_archivo(archivo);
     leer_archivo(archivo);
+    procesar_archivo(archivo,matriz);
+    emitir_reporte(matriz);
 
     fclose(archivo);
     
@@ -59,3 +70,67 @@ void leer_archivo(FILE *f){
     };
     fseek(f,0,SEEK_SET);
 };
+
+void procesar_archivo(FILE *f, NODO *matriz[maxFilas][maxColumnas]){
+
+    // se inicializa la matriz como NULL
+    for(int i=0; i<maxFilas;i++){
+        for(int j=0; j<maxColumnas;j++){
+            matriz[i][j] = NULL;
+        }
+    }
+
+    // pruebas para probar la funcion emitir_reporte, esto se puede borrar
+    // matriz[1][1] = new NODO;
+    // matriz[1][1]->dni = 10;
+    // matriz[1][1]->sig = new NODO;
+    // matriz[1][1]->sig->dni = 20;
+    // matriz[1][1]->sig->sig = NULL;
+
+    // matriz[2][3] = new NODO;
+    // matriz[2][3]->dni = 90;
+    // matriz[2][3]->sig = NULL;
+
+    // matriz[3][5] = new NODO;
+    // matriz[3][5]->dni = 23;
+    // matriz[3][5]->sig = NULL;
+
+    // matriz[3][2] = new NODO;
+    // matriz[3][2]->dni = 50;
+    // matriz[3][2]->sig = new NODO;
+    // matriz[3][2]->sig->dni = 60;
+    // matriz[3][2]->sig->sig = new NODO;
+    // matriz[3][2]->sig->sig->dni = 70;
+    // matriz[3][2]->sig->sig->sig = NULL;
+}
+
+void emitir_reporte(NODO *matriz[maxFilas][maxColumnas]){
+    int reservas[maxFilas];
+    NODO* valor;
+
+    for(int i=0; i<maxFilas;i++){
+        reservas[i] = 0;
+    }
+   
+    for(int i=0; i<maxFilas;i++){
+        for(int j=0; j<maxColumnas;j++){
+            valor = matriz[i][j];
+            while(valor != NULL){
+                reservas[i]++; // Cuenta la cantidad total de reservas por cod_ciudad
+                valor = valor->sig;
+            }
+        }
+        cout << "reserva["<<i<<"]: " << reservas[i] << endl; // muestra la cantidad total de reservas por cod_ciudad
+    }
+
+    // borra la memoria dinamica
+    for(int i=0; i<maxFilas;i++){
+        for(int j=0; j<maxColumnas;j++){
+            while(matriz[i][j] != NULL){
+                valor = matriz[i][j];   
+                matriz[i][j] = matriz[i][j]->sig;
+                delete valor;
+            }
+        }
+    }
+}
